398862e83f7489939d8b4e851a5cc29008374d0b
Sa4ai <ar.eec@mail.ru>
2026-03-18 16:56:45 +0500

    Добавьте файлы проекта.

---
+++ ClientPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace БаязитовЛангуге
+{
+    /// <summary>
+    /// Логика взаимодействия для ClientPage.xaml
+    /// </summary>
+    public partial class ClientPage : Page
+    {
+        int CountRecords;
+        int CountPage;
+        int CurrentPage = 0;
+        List<Client> CurrentPageList = new List<Client>();
+        List<Client> TableList;
+        public ClientPage()
+        {
+            InitializeComponent();
+            var currenClients = БаязитовLanguageEntities.GetContext().Client.ToList();
+            ClientListView.ItemsSource = currenClients;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage());
+        }
+
+        private void LeftDirButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RightDirButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
